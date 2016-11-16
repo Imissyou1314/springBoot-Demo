97f3bc6b88643f79992dc158d540613bd0db3c18
Ground <1255418233@qq.com>
2016-11-16 10:25:28 +0800

    Initial commit

---
+++ springBoot-Demo/src/test/java/com/miss/boot/SendMailTest.java
@@ -0,0 +1,20 @@
+package com.miss.boot;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author:MissYou
+ * @CreateTime:2016/11/15:17:26
+ * @description:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SendMailTest {
+
+}
